b677c72c96417bd5035942ea450b767bc810837a
JMinallah <joviaminallah@gmail.com>
2026-06-10 16:28:33 +0300

    reorganise folder structure and files

---
+++ day2/LabActivity2_login.py
@@ -0,0 +1,6 @@
+#Write a program for Login Authentication that takes a username and password as input and checks if they match the predefined credentials. If they match, print "Login successful", if the username is correct but the password is wrong, print "Invalid password", and if the username is incorrect, print "Username not found".
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+users = [""]
